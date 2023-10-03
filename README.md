70827145f4e97a3afa308accdfac8aa49b997c05
Marius Antonsen <antonsen.marius@gmail.com>
2023-10-03 17:40:04 +0200

    updated readme and fixed issues with time and getting data from ad
    Signed-off-by: Marius Antonsen <antonsen.marius@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# marketplace-metrics-reporter
-Dockerized tool reporting total user counts to a marketplace API 
+# Azure AD Group User Reporter 📊
+
+This application fetches the number of users in an Azure AD group and reports the number to Azure Marketplace Metering Service.
+
+## Environment Variables 🛠️
+
+To run the container, you'll need to provide the following environment variables:
+
+- **TENANT_ID** 🆔
+  - Represents the Tenant ID of your Azure AD.
+
+- **CLIENT_ID** 🔑
+  - The Microsoft Entra Identity Application ID.
+
+- **CLIENT_SECRET** 🤫
+  - Secret associated with the Microsoft Entra Identity Application.
+
+- **RESOURCE** 🌐
+  - URL endpoint of the Azure service you're accessing, typically `https://cloudapi.deployment.marketplace.azure.com/`.
+
+- **METER_ID** 📏
+  - ID that you've set for your metering dimension in Azure Marketplace.
+
+- **CUSTOMER_SUBSCRIPTION_GUID** 📄
+  - Provided when a customer subscribes to your application in Azure Marketplace.
+
+- **RESOURCE_GUID** 🏷️
+  - Your marketplace resource ID.
+
+Please ensure these environment variables are set correctly for the application to function properly.
--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
 import requests
+from datetime import datetime
 
 # Configuration
 TENANT_ID = os.environ.get('TENANT_ID')
-CLIENT_ID = os.environ.get('CLIENT_ID')  # This is the Microsoft Entra Identity Application ID
+CLIENT_ID = os.environ.get('CLIENT_ID')  # Microsoft Entra Identity Application ID
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 RESOURCE = 'https://cloudapi.deployment.marketplace.azure.com/'
 GRANT_TYPE = 'client_credentials'
-METER_ID = os.environ.get('UserCount')  # This is the ID you've set for your metering dimension
-CUSTOMER_SUBSCRIPTION_GUID = os.environ.get('CUSTOMER_SUBSCRIPTION_GUID')  # Provided when a customer subscribes
-RESOURCE_GUID = os.environ.get('RESOURCE_GUID')  # Your marketplace resource ID
+METER_ID = os.environ.get('UserCount')  # ID for metering dimension
+CUSTOMER_SUBSCRIPTION_GUID = os.environ.get('CUSTOMER_SUBSCRIPTION_GUID')  # When a customer subscribes
+RESOURCE_GUID = os.environ.get('RESOURCE_GUID')  # Marketplace resource ID
+GROUP_ID = os.environ.get('GROUP_ID')  # AD group ID
+GRAPH_ENDPOINT = "https://graph.microsoft.com/v1.0/"
 
-# Function to get access token
 def get_access_token():
     token_url = f'https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token'
     token_data = {
@@ -23,22 +25,32 @@ def get_access_token():
     response = requests.post(token_url, data=token_data)
     return response.json().get("access_token")
 
-# Report usage to Azure Marketplace Metering Service
+def get_ad_group_user_count():
+    token = get_access_token()
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(f"{GRAPH_ENDPOINT}groups/{GROUP_ID}/members/$count", headers=headers)
+    if response.status_code == 200:
+        return response.json()['@odata.count']
+    else:
+        print(response.json())
+        return 0
+
 def report_usage(user_count):
     token = get_access_token()
-    
     headers = {
         'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json'
     }
     
     report_url = f'https://cloudapi.deployment.marketplace.azure.com/api/usageEvent?api-version=2018-08-31'
-    
     usage_data = {
         'resourceId': f'/subscriptions/{CUSTOMER_SUBSCRIPTION_GUID}/resourceGroups/default/providers/Marketplace/offerTypes/default/offers/default/plans/default/resources/{RESOURCE_GUID}',
         'quantity': user_count,
         'dimension': METER_ID,
-        'effectiveStartTime': 'UTC_START_TIME',  # e.g., '2021-09-01T00:00:00Z'
+        'effectiveStartTime': datetime.utcnow().isoformat() + 'Z',  # Current UTC time in ISO 8601 format
         'planId': 'default',
         'armRegionName': 'global'
     }
@@ -46,6 +58,7 @@ def report_usage(user_count):
     response = requests.post(report_url, headers=headers, json=usage_data)
     return response.json()
 
-# Example of reporting 10 users
-response = report_usage(10)
+# Report actual user count from AD group to Azure Marketplace
+user_count = get_ad_group_user_count()
+response = report_usage(user_count)
 print(response)
